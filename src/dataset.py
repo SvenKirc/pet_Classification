185c45dcd069685f57773719380dd2113c85f6ad
Sven Kirchner <sven.kirchner@tum.de>
2026-06-08 14:53:17 +0200

    data loading done

---
+++ src/dataset.py
@@ -0,0 +1,42 @@
+from torch.utils.data import Dataset, DataLoader, random_split
+
+import torchvision
+
+import os
+from PIL import Image
+
+from utils import transform
+
+class PetDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        self.img_dir = ("../dataset/images")
+        self.labelledImages = []
+        with open("../dataset/annotations/list.txt", "r") as f:
+            txt = f.read()
+            image_infos = txt.split("\n")[6:-1]
+        for image_info in image_infos:
+            information = image_info.split(" ")
+            self.labelledImages.append([information[0], information[1]])
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir, self.labelledImages[index][0]) + ".jpg"
+        image = transform(Image.open(img_path).convert("RGB"))
+        label = int(self.labelledImages[index][1])
+        return image, label
+    
+    def __len__(self):
+        return len(self.labelledImages)
+
+def main():
+    petDataset = PetDataset()
+    number_training_data = int(2/3 * len(petDataset))
+    number_validation_data = len(petDataset) - number_training_data
+    training_data, validation_data = random_split(petDataset, [number_training_data, number_validation_data])
+    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(validation_data, batch_size=64, shuffle=True)
+    print("GREAT SUCCESS: Dataset loaded")
+    print(f"Samples: {len(petDataset)}")
+
+if __name__ == "__main__":
+    main()
